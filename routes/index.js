a9f24c8dc07644df066fb70896b3c03b03e22904
rajasvyas7 <rajas.vyas@gmail.com>
2022-10-27 10:53:11 +0530

    Testing precommit hook for the ESLint errors

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const public = require('../controllers/publicController');
+const pub = require('../controllers/publicController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  public.index(req, res);
+  pub.index(req, res);
   // res.sendStatus(data);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const public = require('../controllers/publicController');
+const pub = require('../controllers/publicController');
 
 
 router.get('/', function(req, res, next) {
+    let me='Rajas';
   console.log('King Kohli');
   const data = {
     name: 'Rajas',
-    location: 'Pune',
+  location: 'Pune',
   };
   res.sendStatus(data);
 });
 
 router.post('/register', function(req, res, next) {
-  public.register(req, res);
+  pub.register(req, res);
 });
 
 module.exports = router;
